ddf18f33e60ad9248c1982877ec13952dc454b89
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-10-19 01:20:12 +0600

    explicit wait in selenium

--- selenium_free_code_camp/src/main/java/com/demoqa/pages/elements/ElementsPage.java
+++ selenium_free_code_camp/src/main/java/com/demoqa/pages/elements/ElementsPage.java
@@ -3,9 +3,20 @@ package com.demoqa.pages.elements;
 import com.demoqa.pages.HomePage;
 import org.openqa.selenium.By;
 
+import static utilities.JavaScriptUtility.scrollToElementJS;
+
 public class ElementsPage extends HomePage {
     private By webTablesMenuItem = By.xpath("//li[@id=\"item-3\"]/span[text()=\"Web Tables\"]");
     private By linksMenuItem = By.xpath("//li[@id=\"item-5\"]/span[text()=\"Links\"]");
+    private By dynamicPropertiesMenuItem = By.xpath("//li[@id='item-8']/span[text()='Dynamic Properties']");
+    private By textBoxMenuItem = By.xpath("//li[@id='item-0']/span[text()='Text Box']");
+
+
+    public DynamicPropertiesPage clickDynamicProperties() {
+        scrollToElementJS(dynamicPropertiesMenuItem);
+        click(dynamicPropertiesMenuItem);
+        return new DynamicPropertiesPage();
+    }
 
     public WebTablesPage clickWebTables(){
         click(webTablesMenuItem);
---
+++ selenium_free_code_camp/src/test/java/part3_4/com/demoqa/tests/part4/dynamic_wait/DynamicWaitTests.java
@@ -0,0 +1,17 @@
+package part3_4.com.demoqa.tests.part4.dynamic_wait;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import part3_4.com.demoqa.base.BaseTest;
+
+public class DynamicWaitTests extends BaseTest {
+
+    @Test
+    public void testVisibleAfterButtonText()
+    {
+        var dynamicPage = homePage.goToElements().clickDynamicProperties();
+        String actualText = dynamicPage.getVisibleAfterButtonText();
+        String expectedText = "Visible After 5 Seconds";
+        Assert.assertEquals(actualText, expectedText, "\n actual and expected do not match \n");
+    }
+}
