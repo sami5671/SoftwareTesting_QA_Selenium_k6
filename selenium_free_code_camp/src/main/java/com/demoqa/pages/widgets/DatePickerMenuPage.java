0fab1b3ff0b54dc091907527d0352acfe8159d20
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-10-18 17:25:52 +0600

    working with datepicker

---
+++ selenium_free_code_camp/src/main/java/com/demoqa/pages/widgets/DatePickerMenuPage.java
@@ -0,0 +1,42 @@
+package com.demoqa.pages.widgets;
+
+import org.openqa.selenium.By;
+
+import static utilities.DropDownUtility.selectByVisibleText;
+
+public class DatePickerMenuPage extends widgetsPage{
+
+    private By selectDateField = By.id("datePickerMonthYearInput");
+    private By monthDropDown = By.cssSelector(".react-datepicker__month-select");
+    private By yearDropDown = By.cssSelector(".react-datepicker__year-select");
+
+    private By dayValue (String day){
+        return By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day')][text()='"+ day +"']");
+    }
+
+    public void clickDay(String day){
+//        By dayValue = By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day')][text()='"+ day +"']");
+//        click(dayValue);
+        click(dayValue(day));
+    }
+    public boolean isDayInMonth(String day){
+//        By dayValue = By.xpath("//div[contains(@class,'react-datepicker__day react-datepicker__day')][text()='"+ day +"']");
+//        return find(dayValue).isDisplayed();
+        return  find(dayValue(day)).isDisplayed();
+    }
+
+    public void clickSelectDate(){
+        click(selectDateField);
+    }
+
+    public String getDate(){
+        return find(selectDateField).getAttribute("value");
+    }
+
+    public void selectMonth(String month){
+        selectByVisibleText(monthDropDown, month);
+    }
+    public void selectYear(String year){
+        selectByVisibleText(yearDropDown, year);
+    }
+}
