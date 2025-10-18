1a80b315ea41799bd61feb9acd2edb0b21f589fa
sami5671 <112261102+sami5671@users.noreply.github.com>
2025-10-19 02:14:02 +0600

    simulate the mouse event

--- selenium_free_code_camp/src/main/java/com/demoqa/pages/widgets/widgetsPage.java
+++ selenium_free_code_camp/src/main/java/com/demoqa/pages/widgets/widgetsPage.java
@@ -9,7 +9,20 @@ public class widgetsPage extends HomePage {
 
     private By selectMenuItem = By.xpath("//li[@id='item-8']/span[text()='Select Menu']");
     private By datePickerMenuItem = By.xpath("//li[@id='item-2']/span[text()='Date Picker']");
+    private By progressBarMenuItem = By.xpath("//li[@id='item-4']/span[text()='Progress Bar']");
+    private By sliderMenuItem = By.xpath("//li[@id='item-3']/span[text()='Slider']");
 
+    public SliderPage clickSlider() {
+        scrollToElementJS(sliderMenuItem);
+        click(sliderMenuItem);
+        return new SliderPage();
+    }
+
+    public ProgressBarPage clickProgressBar() {
+        scrollToElementJS(progressBarMenuItem);
+        click(progressBarMenuItem);
+        return new ProgressBarPage();
+    }
 
     public SelectMenuPage clickSelectMenu(){
         scrollToElementJS(selectMenuItem);
--- selenium_free_code_camp/src/test/java/part3_4/com/demoqa/tests/part4/dynamic_wait/DynamicWaitTests.java
+++ selenium_free_code_camp/src/test/java/part3_4/com/demoqa/tests/part4/dynamic_wait/DynamicWaitTests.java
@@ -4,14 +4,23 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import part3_4.com.demoqa.base.BaseTest;
 
+@Test
 public class DynamicWaitTests extends BaseTest {
 
-    @Test
     public void testVisibleAfterButtonText()
     {
         var dynamicPage = homePage.goToElements().clickDynamicProperties();
         String actualText = dynamicPage.getVisibleAfterButtonText();
         String expectedText = "Visible After 5 Seconds";
         Assert.assertEquals(actualText, expectedText, "\n actual and expected do not match \n");
+    }
+
+    public void testProgressBar() {
+        var progressBarPage = homePage.goToWidgets().clickProgressBar();
+        progressBarPage.clickStartButton();
+        String actualValue = progressBarPage.getProgressValue();
+        String expectedValue = "100%";
+        Assert.assertEquals(actualValue, expectedValue,
+                "\n Value Is Not 100% \n");
     }
 }
